819a8bbe1fa005201949f145377a8e083394dbfb
Manuel Morfin <ing.manuel.morfin@gmail.com>
2025-06-02 11:24:55 -0600

    33

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: resolve(__dirname, 'public/index.html') // 👈 apunta explícitamente al index.html
-    }
-  }
 });
